ae5bd9c4fd3e25d18c5f7b76913e7f2d7da0a5a8
MatheusNunes <matheusnunes9147@gmail.com>
2022-07-13 17:23:02 -0300

    Date Select

--- backend/src/main/java/com/snmatheus/dsmeta/repositories/SaleRepository.java
+++ backend/src/main/java/com/snmatheus/dsmeta/repositories/SaleRepository.java
@@ -3,10 +3,18 @@ Ex: Consultar, Salvar, Atualizar, Deletar uma venda.
 */
 package com.snmatheus.dsmeta.repositories;
 
+import java.time.LocalDate;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.snmatheus.dsmeta.entities.Sale;
 
 public interface SaleRepository extends JpaRepository<Sale, Long>{
+	
+	@Query("SELECT obj FROM Sale obj WHERE obj.date BETWEEN :min AND :max ORDER BY obj.amount DESC")
+	Page<Sale> findSales(LocalDate min, LocalDate max, Pageable pageable);
 	
 }
